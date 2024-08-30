1b420c72e1a10565f25e509033024bf301750e5e
rushikesh <rushikeshk@outbound.ai>
2024-08-30 16:54:37 +0530

    remove terraform warnings

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "storage" {
   account_kind                    = "Storage"
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
-  enable_https_traffic_only       = true
+  https_traffic_only_enabled      = true
   allow_nested_items_to_be_public = false
   min_tls_version                 = "TLS1_2"
 
